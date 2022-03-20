3e1d83ef27a8d8c1f56588b5ed48a3b2cb2a387f
Meir Sedlis <meirsedlis@gmail.com>
2022-03-19 22:51:14 -0500

    wrote psuedo code

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,24 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+
+let basket = [];
+let item = 'banana';
+
+// CREATE FUNCTION addItem
+function addIdtem('item')
+// INPUT: parameter string 'item'
+// CODE adding the idem to the cart
+// OUPUT: return true that the item was added
+
+// CREATE FUNCTION listItems
+// INPUT: the basket array
+// OUTPUT: a console.log of each individual item on a new line
+
+// CREATE FUNCTION empty
+
+function empty(){
+    basket= [];
+} // end function empty
+
